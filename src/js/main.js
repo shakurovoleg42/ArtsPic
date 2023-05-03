3bfc6672a8df05ab177ea4c9c4e0a36881d9576e
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-05-03 15:20:19 +0500

    modified:   dist/script.js 	modified:   dist/script.js.map 	modified:   gulpfile.js 	modified:   src/js/main.js 	new file:   src/js/modules/checkTextInputs.js 	new file:   src/js/modules/mask.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import forms from "./modules/forms";
-
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -10,4 +11,8 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
+    mask("[name='phone']");
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+
 });
